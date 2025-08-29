883dc0efee33a6c89d6612111746262fd3270866
Razib Das <it.razib.mbstu@gmail.com>
2025-08-29 10:32:53 +0600

    Added Call button Functionality

--- js/script.js
+++ js/script.js
@@ -1,12 +1,42 @@
 // heart button click
 console.log("Added js")
-// heart buttons click using event deligation heart count
 let count = 0;
-document.getElementById("heart-btn-container").addEventListener("click",function(event){
-     const displayCount = document.getElementById("heart-count");
+// count the coin
+function getCoinCounter(){
     
-    if(event.target.classList.contains('heart-btn')){
-       count++;
+             
+}
+// count the heart
+function getHeartConter(){
+   const displayCount = document.getElementById("heart-count");
+    count++;
        displayCount.innerText = count;
+}
+// heart buttons click using event deligation heart count
+document.getElementById("btn-container").addEventListener("click",function(event){
+     
+    if(event.target.classList.contains('heart-btn')){
+      getHeartConter();
     }
 })
+const buttons = document.querySelectorAll('.call-btn');
+for(const button of buttons){
+    button.addEventListener("click", function(){
+       const inner =  button.closest('.card');
+       const heading = inner.querySelector('.heading').innerText;
+       const number = inner.querySelector('.number').innerText;
+    //   coin counter
+     const displayCoin = document.getElementById("coin-count");
+     const coinCounter = parseInt(displayCoin.innerText)
+     const counter = coinCounter;
+      if(counter != 0 ){
+         alert(`📞 Calling ${heading} ${number}...`)
+        const counter = coinCounter - 20;
+          displayCoin.innerText = counter; 
+      }
+      else{
+            alert("❌ আপনার পর্যাপ্ত কয়েন নেই কল করতে কমপক্ষে ২০ কয়েন লাগবে।")
+      }
+       
+    })
+}
